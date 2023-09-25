8c661efd902f15f16e2d74f1fba4bb7f1cec672c
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-25 14:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
+	 if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    // Append the remainder of num divided by 2 to the binary string
+    binary = (num % 2) + binary;
+    // Divide num by 2 for the next iteration
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
   
 }
 
